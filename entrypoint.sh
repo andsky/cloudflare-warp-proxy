7d50c0639d0e2fa21d95ec0217a32a4ac6ddfcee
andsky <git@6w.pw>
2024-04-10 11:55:29 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,14 @@ while [[ ! -S /run/cloudflare-warp/warp_service ]]; do sleep 1; done
 
 
 while [[ ! -f /var/lib/cloudflare-warp/reg.json ]]; do
-    warp-cli --accept-tos register || sleep 5
+    warp-cli --accept-tos registration new || sleep 5
 done
 
-warp-cli --accept-tos set-mode proxy
-warp-cli --accept-tos set-proxy-port 40001
+warp-cli --accept-tos mode proxy
+warp-cli --accept-tos proxy port 40001
 
 warp-cli --accept-tos connect
-warp-cli --accept-tos disable-dns-log
-warp-cli --accept-tos enable-always-on
+warp-cli --accept-tos dns log disable
+#warp-cli --accept-tos enable-always-on
 
 socat TCP-LISTEN:40000,fork,reuseaddr TCP:127.0.0.1:40001
